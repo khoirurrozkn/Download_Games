39c159ae863d7c100951ce560cfa1d95350c651a
khoirurrozkn <irurkampus@gmail.com>
2023-05-27 20:10:15 +0700

    menghapus testing

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NotFound from './pages/NotFound'
 import Hero from './pages/Hero'
-import Test from './pages/Test'
 
 function App() {
   return(
@@ -10,7 +9,6 @@ function App() {
       <div id='app'>
         <Routes>
           <Route exact path='/' element={<Hero />}></Route>
-          <Route exact path='/test' element={<Test />}></Route>
           <Route path='/*' element={<NotFound />}></Route>
         </Routes>
       </div>
