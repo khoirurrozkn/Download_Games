e09d8e7517d2eddca592f58b4e3097f358081812
khoirurrozkn <irurkampus@gmail.com>
2023-05-29 02:30:20 +0700

    config

--- server/index.js
+++ server/index.js
@@ -5,22 +5,35 @@ import { notFound, errorHandler } from './middleware/errorHandler.js'
 import connection from './config/database.js'
 import categoryRoute from './routes/category.js'
 import gameCategory from './routes/games.js'
+import path from 'path'
+
 
 dotenv.config()
 const app = express()
 connection()
-app.use(cors())
+app.use(cors({origin: 'https://kukigames.vercel.app'}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/category', categoryRoute)
 app.use('/api/game', gameCategory)
-app.use('/*', (req, res) => {
-    res.status(404).send("hayoo mau ngapain")
-})
-app.use('/', (req, res) => {
-    res.status(200).send("api-berjalan")
-})
+
+////////////////////////////////// DEPLOY
+
+const __dirname1 = path.resolve()
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1, '/client/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname1, 'client', 'build', 'index.html'))
+    })
+}else{
+    app.get('/', (req, res) => {
+        res.status(200).send('api berjalan')
+    })
+}
+
+////////////////////////////////// DEPLOY
 
 app.use(notFound)
 app.use(errorHandler)
