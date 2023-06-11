b0c369005b8195b9665d90bb40673be6b27dc9f3
khoirurrozkn <irurkampus@gmail.com>
2023-06-11 22:04:28 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import { notFound, errorHandler } from './middleware/errorHandler.js'
 import connection from './config/database.js'
 import categoryRoute from './routes/category.js'
 import gameCategory from './routes/games.js'
-import path from 'path'
+// import path from 'path'
 
 
 dotenv.config()
@@ -20,17 +20,21 @@ app.use('/api/game', gameCategory)
 
 ////////////////////////////////// DEPLOY
 
-const __dirname1 = path.resolve()
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname1, '/client/build')))
+// const __dirname1 = path.resolve()
+// if(process.env.NODE_ENV==='production'){
+//     app.use(express.static(path.join(__dirname1, '/client/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
-    })
-}else{
-    app.get('/', (req, res) => {
-        res.status(200).send('api berjalan')
-    })
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
+//     })
+// }else{
+//     app.get('/', (req, res) => {
+//         res.status(200).send('api berjalan')
+//     })
+// }
+
+app.get('/', (req, res) => {
+    res.status(200).send('api berjalan')
 }
 
 ////////////////////////////////// DEPLOY
