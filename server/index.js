873635cb7d969409caeec845a81674d025a5d533
khoirurrozkn <irurkampus@gmail.com>
2023-06-11 22:33:41 +0700

    update index

--- server/index.js
+++ server/index.js
@@ -5,13 +5,11 @@ import { notFound, errorHandler } from './middleware/errorHandler.js'
 import connection from './config/database.js'
 import categoryRoute from './routes/category.js'
 import gameCategory from './routes/games.js'
-import path from 'path'
-
 
 dotenv.config()
 const app = express()
 connection()
-app.use(cors({origin: 'https://zigames.vercel.app/'}))
+app.use(cors({ origin: 'https://zigames.vercel.app/' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -20,16 +18,17 @@ app.use('/api/game', gameCategory)
 
 ////////////////////////////////// DEPLOY
 
-const __dirname1 = path.resolve()
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname1, '/client/build')))
+if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the 'client' folder
+    app.use(express.static('client'))
 
+    // Catch-all route for client-side routing
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
     })
-}else{
+} else {
     app.get('/', (req, res) => {
-        res.status(200).send('api berjalan')
+        res.status(200).send('API berjalan')
     })
 }
 
@@ -38,4 +37,4 @@ if(process.env.NODE_ENV==='production'){
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(process.env.PORT ,() => console.log(`Server running on PORT ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`Server running on PORT ${process.env.PORT}`))
