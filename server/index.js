2791ceffc0cac86b7080a201291022f03b542277
khoirurrozkn <irurkampus@gmail.com>
2023-06-11 22:41:38 +0700

    updated

--- server/index.js
+++ server/index.js
@@ -22,10 +22,10 @@ app.use('/api/game', gameCategory)
 
 const __dirname1 = path.resolve()
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname1, '/client/public')))
+    app.use(express.static(path.join(__dirname1, '/client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'client', 'public', 'index.html'))
+        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
     })
 }else{
     app.get('/', (req, res) => {
