48f8218dca4d94ef943d934c5fc79991d6fe0501
khoirurrozkn <irurkampus@gmail.com>
2023-06-15 18:34:06 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import { notFound, errorHandler } from './middleware/errorHandler.js'
+import { notFound } from './middleware/errorHandler.js'
 import connection from './config/database.js'
 import categoryRoute from './routes/category.js'
 import gameCategory from './routes/games.js'
-import path from 'path'
+// import path from 'path'
 
 
 dotenv.config()
@@ -21,19 +21,19 @@ app.use('/api/game', gameCategory)
 
 ////////////////////////////////// DEPLOY
 
-const __dirname = path.resolve()
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.resolve(__dirname, '../client/build')));
+// const __dirname = path.resolve()
+// if(process.env.NODE_ENV==='production'){
+//     app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-    // Route all requests to the client-side index.html
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-    });
-}
+//     // Route all requests to the client-side index.html
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+//     });
+// }
 
 ////////////////////////////////// DEPLOY
 
 app.use(notFound)
-app.use(errorHandler)
+// app.use(errorHandler)
 
 app.listen(process.env.PORT ,() => console.log(`Server running on PORT ${process.env.PORT}`))
--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -4,16 +4,16 @@ const notFound = (req, res, next) => {
     next(error)
 }
 
-const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
+// const errorHandler = (err, req, res, next) => {
+//     const statusCode = res.statusCode ? res.statusCode : 500
 
-    res.status(statusCode).json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
-    })
-}
+//     res.status(statusCode).json({
+//         message: err.message,
+//         stack: process.env.NODE_ENV === 'production' ? null : err.stack
+//     })
+// }
 
 export {
     notFound,
-    errorHandler
+    // errorHandler
 }
