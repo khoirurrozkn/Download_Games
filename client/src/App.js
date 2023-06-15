facd04eb3e6d2b75d2c109b7f0e254e32bdea324
khoirurrozkn <irurkampus@gmail.com>
2023-06-15 20:30:55 +0700

    reUi

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
+import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NotFound from './pages/NotFound'
 import Hero from './pages/Hero'
+import Category from './pages/Category'
 
 function App() {
   return(
     <BrowserRouter>
-      <div id='app'>
-        <Routes>
-          <Route exact path='/' element={<Hero />}></Route>
-          <Route path='/*' element={<NotFound />}></Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route exact path='/' element={<Hero />}></Route>
+        <Route exact path='category' element={<Category/>}></Route>
+        <Route path='/*' element={<NotFound />}></Route>
+      </Routes>
     </BrowserRouter>
   )
 }
