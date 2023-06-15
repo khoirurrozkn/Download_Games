461ad6bc60910eb553874da771ebd408681196dc
khoirurrozkn <irurkampus@gmail.com>
2023-06-15 23:49:21 +0700

    update logo

---
+++ client/src/component/Navbar.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Button, Offcanvas, Container } from 'react-bootstrap'
+import './Navbar.css'
+
+const Navbar = (props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Container fluid className='p-0 fixed-top d-flex justify-content-end align-items-center' style={{
+        height: '60px'
+    }}>
+      <Button onClick={handleShow} className="d-block text-dark fontNav bg-light">
+        <i className="bi bi-list-ul"></i>
+      </Button>
+      <Offcanvas placement='top' show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>{props.title}</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+            {props.body}
+        </Offcanvas.Body>
+      </Offcanvas>
+    </Container>
+  )
+}
+
+export default Navbar
--- client/src/pages/Hero.js
+++ client/src/pages/Hero.js
@@ -1,5 +1,8 @@
 import React,{ useState, useEffect } from 'react'
 import games from './../game/Game'
+import { Container } from 'react-bootstrap'
+import Navbar from './../component/Navbar'
+import './Hero.css'
 
 const Hero = () => {
   const [ data, setData ] = useState([])
@@ -13,7 +16,33 @@ const Hero = () => {
   },[])
 
   return (
-    <div>Hero</div>
+    <Container fluid className='p-0' style={{
+      height: 'auto'
+    }}>
+      <Navbar title='Pilihan' body='babayo babayo babayo babayo babayo babayo babayo babayo babayo babayo babayo babayo babayo babayo babayo babayo' />
+
+      <div className='w-100 btrans' style={{height: 'auto', backgroundColor: 'rgb(22,27,34)'}}>
+        <h1 className='text-center' style={{
+          fontFamily: "'Nanum Myeongjo', serif", 
+          letterSpacing: '2px',
+          color: 'rgba(255, 255, 255, 0.918)'
+        }}>
+          Roz Games
+        </h1>
+          <div className='caption w-100 d-flex justify-content-center'>
+            <p className='text-light d-block text-center'>
+              Roz Games menyediakan link game-game terbaru, serta memberikan rekomendasi 
+              tentang game-game populer dan terkini. 
+              Anda dapat menjelajahi koleksi game PC dengan akses yang mudah dan cepat.
+            </p>
+          </div>
+      </div>
+      <svg className='waveTop' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#161B22" fill-opacity="1" d="M0,192L48,208C96,224,192,256,288,245.3C384,235,480,181,576,154.7C672,128,768,128,864,160C960,192,1056,256,1152,272C1248,288,1344,256,1392,240L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+      <div className='w-100 content' style={{backgroundColor: 'rgb(13,17,22)', height: 'auto'}}>
+        
+      </div>
+    </Container>
   )
 }
 
